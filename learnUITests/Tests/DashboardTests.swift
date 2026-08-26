258a8f3dac154cc18eaef420d4a33c00c9402abf
Cindy Michalowski <cindymichalowski.tech@gmail.com>
2026-08-26 10:30:51 -0400

    Moved Performance Tests to their own class

--- learnUITests/Tests/DashboardTests.swift
+++ learnUITests/Tests/DashboardTests.swift
@@ -9,14 +9,6 @@ import XCTest
 
 final class DashboardTests: BaseTest {
 
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
-    }
-    
     func testDashboardDisplayedAsExpectedOnLaunch() throws {
         DashboardScreen()
             .isDisplayedAsExpectedOnLaunch()
